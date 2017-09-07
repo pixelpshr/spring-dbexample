6c8eb34753990071fac34d8ee5540fe42f8ecbb1
Felix Roske <f.roske@roskenet.de>
2017-09-07 10:22:12 +0200

    Play with the repository

--- src/main/java/greenfield/GreenfieldControllerImpl.java
+++ src/main/java/greenfield/GreenfieldControllerImpl.java
@@ -1,7 +1,10 @@
 package greenfield;
 
+import java.sql.Timestamp;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.event.Event;
@@ -9,11 +12,24 @@ import javafx.event.Event;
 @FXMLController
 public class GreenfieldControllerImpl implements GreenfieldController {
     private static Logger LOGGER = LoggerFactory.getLogger(GreenfieldControllerImpl.class);
+   
+    @Autowired
+    private RecordRepository repository;
     
     @Override
     public void handleFileOpenAction(Event event) {
-        LOGGER.debug("FileOpen");
+        // Playing around: Saving a record:
+        Record newRecord = new Record();
+        newRecord.setDatabundle("Something");
+        newRecord.setBeginTs(new Timestamp(0));
         
+        repository.save(newRecord);
+        
+        // Getting it back from the db:
+        
+        Iterable<Record> all = repository.findAll();
+        
+        all.forEach(r -> LOGGER.debug(r.toString()));
     }
 
 }
